38559044862758b00d896d1a042a9ee434343242
Jim Ivanoff <jim@polarbeardesign.net>
2014-12-16 13:04:55 -0600

    updates to copy, added sponsor info in sidebar

--- index.php
+++ index.php
@@ -34,58 +34,19 @@ session_start();
       <img src="img/about.png" width="251" height="84" alt="about scipy" class="hp_image" />
     <p>The annual SciPy Conference allows participants from academic, commercial, and governmental organizations to showcase their latest Scientific Python projects, learn from skilled users and developers, and collaborate on code development.</p>
 
-    <p>We look forward to a very exciting conference and hope to see you at the 2015 conference.</p>
+    <p>Conference registration to open January 2015.</p>
   </div>
   <div class="hp_cell" style="margin: 0 2%;">
     <h2>Schedule</h2>
-        <p>The 2015 conference will, preliminarily, consist of two days of tutorials followed by two days of presentations, and concludes with two days of developer sprints on projects of interest to the attendees.</p>
-        <?php echo $display_dates ?>
-        <div class="row" style="margin: 0;">
-          <div class="free_cell">
-            <div class="icon_date"><p>Jul</p>
-            <p class="icon_date_w">Mon</p>
-            <p class="icon_date_day">6</p></div>
-          </div>
-          <div class="free_cell">
-            <div class="icon_date"><p>Jul</p>
-            <p class="icon_date_w">Tue</p>
-            <p class="icon_date_day">7</p></div>
-          </div>
-          <div class="free_cell" style="max-width: 20%; padding-top: 1em;">
-            Tutorials
-          </div>
-        </div>
-        <div class="row" style="margin: 0;">
-          <div class="free_cell">
-            <div class="icon_date"><p>Jul</p>
-            <p class="icon_date_w">Wed</p>
-            <p class="icon_date_day">8</p></div>
-          </div>
-          <div class="free_cell">
-            <div class="icon_date"><p>Jul</p>
-            <p class="icon_date_w">Fri</p>
-            <p class="icon_date_day">10</p></div>
-          </div>
-          <div class="free_cell" style="max-width: 50%; padding-top: 1em;">
-            Main Conference Session
-          </div>
-        </div>
-        <div class="row" style="margin: 0;">
-          <div class="free_cell">
-            <div class="icon_date"><p>Jul</p>
-            <p class="icon_date_w">Sat</p>
-            <p class="icon_date_day">11</p></div>
-          </div>
-          <div class="free_cell">
-            <div class="icon_date"><p>Jul</p>
-            <p class="icon_date_w">Sun</p>
-            <p class="icon_date_day">12</p></div>
-          </div>
-          <div class="free_cell" style="max-width: 20%; padding-top: 1em;">
-            Sprints
-          </div>
-        </div>
-        <p style="clear: both;">Look for more information to come.</p>
+        <p>The 2015 conference will consist of two days of tutorials followed by three days of presentations, and conclude with two days of developer sprints on projects of interest to attendees.</p>
+        
+        <p style="text-indent: 1em;">Tutorials: <strong>July 6 - 7, 2015</strong></p>
+        
+        <p style="text-indent: 1em;">Main Conference: <strong>July 8 - 10, 2015</strong></p>
+        
+        <p style="text-indent: 1em;">Sprints: <strong>July 11 - 12, 2015</strong></p>
+        
+        <p style="clear: both;">Program and schedule details coming soon.</p>
   </div>
   <div class="hp_cell">
     <h2>What's it like?</h2>
